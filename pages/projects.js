3bec337c49f2f0efaaf768318743f027a9c3b778
nick adams <thebigdeal916@icloud.com>
2021-12-24 14:00:06 -0600

    took away cols and rows in projects page

--- pages/projects.js
+++ pages/projects.js
@@ -2,9 +2,6 @@ import Layout from "../Components/Layout";
 import projects1 from "./projects.json";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -14,23 +11,19 @@ const projects = () => {
         <>
             <Layout>
                 <div className="body">
-                    <div style={{display: 'flex'}}>
+                    <div style={{ display: 'flex' }}>
                         {projects1.map(project => {
                             return (
-                                <Container fluid>
-                                    <Row>
-                                        <Col style={{flexDirection: 'column'}} xs={12} sm={12} md={6}>
-                                            <Card style={{ width: '18rem', margin: '5px' }} id={project.id} id="card">
-                                                <Card.Img variant="top" src={project.image} />
-                                                <Card.Body>
-                                                    <Card.Title>{project.title}</Card.Title>
-                                                    <Card.Text>{project.description}</Card.Text>
-                                                    <Button variant="outline-secondary" href={project.link} className="btn" style={{ marginLeft: '28%'}}>View Here</Button>
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
-                                    </Row>
-                                </Container>
+                                <div id="card-body" key={project.id}>
+                                    <Card style={{ width: '18rem', margin: '5px' }} id="card">
+                                        <Card.Img variant="top" src={project.image} />
+                                        <Card.Body>
+                                            <Card.Title>{project.title}</Card.Title>
+                                            <Card.Text>{project.description}</Card.Text>
+                                            <Button variant="outline-secondary" href={project.link} className="btn" style={{ marginLeft: '28%' }}>View Here</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
                             )
                         })}
                     </div>
@@ -42,7 +35,13 @@ const projects = () => {
                  .body {
                      background-color: black;
                      height: 100%;
-                     width:100%;
+                     width: 100%;
+                     display: inline-block;
+                 }
+
+                 #card-body {
+                     background-color: green;
+                     width: 100%;
                  }
    
             `}
