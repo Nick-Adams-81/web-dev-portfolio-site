a936bc398b70a92ac6fd2f76239cb84d2f85c354
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-24 20:41:34 -0500

    updated hookbin url

--- Components/form.js
+++ Components/form.js
@@ -12,7 +12,7 @@ const Forms = () => {
     const [email, setEmail] = useState("");
     const [comments, setComments] = useState("");
 
-    const url = "https://hookb.in/yDOJwRWQnGhGkPZGamwM";
+    const url = "https://hookb.in/Oenokyzrr8Hq0lmqkdqg";
 
     const handleSubmit = (e) => {
 
