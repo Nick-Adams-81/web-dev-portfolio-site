8f22bb695f0b24768668e11fc7596421a4959d74
Nick Adams <nick.pappas.adams@gmail.com>
2022-08-27 16:14:33 -0700

    updated homepage info

--- Components/HomeComponents/HomeInfo.js
+++ Components/HomeComponents/HomeInfo.js
@@ -5,7 +5,7 @@ const HomeInfo = () => {
         <>
             <div>
                 <h2 className="animate__animated animate__fadeIn">About me</h2>
-                <h4 className="animate__animated animate__fadeIn">I am a software developer based out of Dallas, TX, I strive to make software that solves everyday issues
+                <h4 className="animate__animated animate__fadeIn">I am a software developer specialiaing in MERN stack, and Java Spring/boot development. I strive to make software that solves everyday issues
                     people encounter, links to my projects are available in the portfolio page! Get in touch with me on the connect page!
                 </h4>
             </div>
